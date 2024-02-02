529ec735970cc8f4edba1cf2e27a7c84b2a425ac
Gorgees Odisho <gorgees041@gmail.com>
2024-02-02 21:20:46 +0300

    create ShopItem schema #4

---
+++ server/versions/v1/models/shopItemModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const ShopItem = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  availableCount: {
+    type: Number,
+    default: 1,
+  },
+  category: {
+    type: [String],
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("shopitem", ShopItem);
