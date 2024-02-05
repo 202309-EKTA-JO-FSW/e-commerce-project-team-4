fcb36235f304d1c3d8c1a9e8d58bf9e7c4e80ee3
Gorgees Odisho <gorgees041@gmail.com>
2024-02-06 00:04:08 +0300

    fix some lines

--- server/versions/v1/controllers/adminController.js
+++ server/versions/v1/controllers/adminController.js
@@ -1,65 +1,57 @@
 const ShopItem = require("../models/shopItemModel");
 
-//const addNewItem = (req, res) => {};
-
-// add a new item to the shop
-
 async function addNewItem(req, res) {
-    try { 
-        const {title,price,description,availableCount,category,image}= req.body
+  try {
+    const { title, price, description, availableCount, category, image } =
+      req.body;
 
-        const newItem = new ShopItem({
-            title,
-            price,
-            description,
-            availableCount,
-            category,
-            image // the only case I could make it work is "Assuming theres a URL pointing to the image"
-        });
-        
-        const savedItem = await newItem.save();
+    const newItem = new ShopItem({
+      title,
+      price,
+      description,
+      availableCount,
+      category,
+    });
 
-        res.status(201).json({message: "Item added sucessfully", item: savedItem});
-    
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+    const savedItem = await newItem.save();
+
+    res
+      .status(201)
+      .json({ message: "Item added sucessfully", item: savedItem });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 //update an existing ShopItem:
 
 async function updateItem(req, res) {
-    try {
-        const {id} = req.params;
-        const {title, price,description,availableCount,category,image} = req.body;
+  try {
+    const { id } = req.params;
+    const { title, price, description, availableCount, category, image } =
+      req.body;
 
-        const updatedItem = await ShopItem.findByIdAndUpdate(
-            id,
-            {
-                title,
-                price,
-                description,
-                availableCount,
-                category,
-                image // the only case I could make it work is "Assuming theres a URL pointing to the image"
-            },
-            {new: true}
-            );
-        if (updatedItem) {
-            res.json({message: "Item updated sucessfully!", item: updatedItem})
-
-        } else {
-            res.status(404).json({ error: "Item not found"});
-        }           
-        
-    } catch (error) {
-        res.status(500).json({message: error.message});
-        
+    const updatedItem = await ShopItem.findByIdAndUpdate(
+      id,
+      {
+        title,
+        price,
+        description,
+        availableCount,
+        category,
+      },
+      { new: true }
+    );
+    if (updatedItem) {
+      res.json({ message: "Item updated sucessfully!", item: updatedItem });
+    } else {
+      res.status(404).json({ error: "Item not found" });
     }
-    
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-
 module.exports = {
-    addNewItem,
-    updateItem
-  };
+  addNewItem,
+  updateItem,
+};
