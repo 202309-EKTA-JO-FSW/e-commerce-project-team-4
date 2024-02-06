e738e7705602ce93e8a46bb12f1cfc66f29395a2
Mohamad Karbejha <78583194+Karbejha@users.noreply.github.com>
2024-02-06 14:32:56 +0300

    Edits in server , routes , errors and controller

--- server/index.js
+++ server/index.js
@@ -10,10 +10,16 @@ const port =
     ? process.env.NODE_LOCAL_TEST_PORT
     : process.env.NODE_LOCAL_PORT;
 
+const errorHandlers = require('./versions/v1/utils/errorHandlers')
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/v1/api/", adminRouter);
+
+app.use(errorHandlers.handleValidationErrors);
+app.use(errorHandlers.handleDuplicateKeyError);
+app.use(errorHandlers.handleGeneralError);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
